15348f82b502694af6fe2ed0232f8a66b70acb5f
Montodel <suleimanmoha120@gmail.com>
2023-04-26 11:19:46 +0000

    script that prints the title of a Star Wars movie

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,11 @@
- #!/usr/bin/node
+#!/usr/bin/node
 const request = require('request');
-const { argv } = require('process');
+const end = process.argv[2];
 
-const BaseUrl = 'https://swapi-api.hbtn.io/api';
-request(BaseUrl + '/films/' + argv[2], (error, response, body) => {
+request('https://swapi-api.hbtn.io/api/films/' + end, function (error, respones, body) {
   if (error) {
-    console.error(error);
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
   }
-  console.log(JSON.parse(body).title);
 });
